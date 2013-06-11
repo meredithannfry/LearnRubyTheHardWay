4c4fe726449cb8f591e05e6db80ee5eb7e34225e
Meredith Fry <meredithannfry@gmail.com>
2013-06-11 19:58:54 +1000

    Exercise 1 String manipulation

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+# To run ruby ex1.rb in terminal
+# Octothorpe....Pound... represents a comment..duh
+
+puts "Hello World"
+puts "Pete is a wombat"
+puts "Ok, I'm bored now"
+
+puts "Pete is still a numbnut in reverse: " + " Pete is a numbnut".reverse
+puts "Pete is a wombat" * 10
