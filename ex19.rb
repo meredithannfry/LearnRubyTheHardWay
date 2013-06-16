b16ddea4075c5e684a638aff3ad3001e7d784c9c
Meredith Fry <meredithannfry@gmail.com>
2013-06-17 07:55:20 +1000

    Exercise 19 - more examples of user defined function and ways to invoke it.

---
+++ ex19.rb
@@ -0,0 +1,30 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  puts "You have #{cheese_count} cheeses!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
+  puts "Man that's enough for a party!"
+  puts "Get a blanket."
+  puts # a blank line
+end
+
+puts "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+puts "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+puts "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+puts "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+# taking user input for cheese and crackers
+puts "I am going to try to use user defined variables. See what that does:"
+puts "Give me a number of cheeses: "
+number_cheese = gets.chomp()
+puts "Give me a number of crackers: "
+number_cracker = gets.chomp()
+cheese_and_crackers(number_cheese, number_cracker)
+
